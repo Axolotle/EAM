28462a26417d5e9eaebbac5cbb1281fe8df7c331
axolotle <nchesnais@mailoo.org>
2021-02-05 15:14:43 +0100

    add texts and setup assets file structure

--- app/routes/upload.py
+++ app/routes/upload.py
@@ -8,16 +8,16 @@ bp = Blueprint('upload', __name__)
 
 @bp.route('/', methods=['POST'])
 def upload():
-    sounds_path = os.path.join(current_app.root_path, 'static', 'sounds')
+    assets_path = os.path.join(current_app.root_path, 'static', 'assets')
     file = request.files.get('file')
     filename = time.strftime('%Y%m%d-%Hh%Mm%Ss') + '.ogg'
-    filepath = os.path.join(sounds_path, 'contributiosns', filename)
+    filepath = os.path.join(assets_path, 'contributiosns', filename)
     try:
         file.save(filepath)
     except:
         return ('ERROR', 500)
 
-    with open(os.path.join(sounds_path, 'contributions.cvs'), 'a') as contrib:
+    with open(os.path.join(assets_path, 'contributions.cvs'), 'a') as contrib:
         contrib.write(filename + '\n')
 
     return ('OK', 200)
