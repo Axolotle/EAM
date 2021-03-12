71f5780290370b3afcaf04284049dca67182af29
axolotle <nchesnais@mailoo.org>
2021-03-12 13:10:30 +0100

    update main App with views

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,14 +1,36 @@
 <template>
   <div id="app">
+    <p>next: {{ nextEp }}</p>
+    <p>nextDate: {{ nextEpDate }}</p>
+
+    <component :is="component" v-if="component" />
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import Episode from '@/views/Episode'
+import Street from '@/views/Street'
+
 
 export default {
-  name: 'App'
+  name: 'App',
+
+  components: {
+    Episode,
+    Street
+  },
+
+  computed: {
+    ...mapGetters(['nextEpDate', 'nextEp', 'component'])
+  },
+
+  created () {
+    this.$store.dispatch('INIT')
+  }
 }
 </script>
 
 <style lang="scss">
+@import '@/assets/app.scss';
 </style>
