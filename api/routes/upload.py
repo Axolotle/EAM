426b57de8af046077ddeb7332d65b8a3b1c01fe5
axolotle <nchesnais@mailoo.org>
2021-04-05 17:39:55 +0200

    add basic checks

--- api/routes/upload.py
+++ api/routes/upload.py
@@ -8,17 +8,22 @@ bp = Blueprint('upload', __name__)
 
 @bp.route('/', methods=['POST'])
 def upload():
-    return ('NOPE', 500)
     assets_path = os.path.join(current_app.root_path, 'static', 'assets')
     file = request.files.get('file')
-    filename = time.strftime('%Y%m%d-%Hh%Mm%Ss') + '.ogg'
+    ext = file.mimetype.replace('audio/', '')
+    filename = time.strftime('%Y%m%d-%Hh%Mm%Ss') + '.' + ext
     filepath = os.path.join(assets_path, 'contributions', filename)
     try:
+        assert current_app.config['REFERER'] in request.headers['Referer']
+        assert current_app.config['PS'] == request.form['ps']
+        assert ext in ('ogg', 'webm')
+        assert 0 < float(request.form['duration']) <= 61
+        assert int(request.form['size']) < 1500000
+        assert file.mimetype_params['codecs'] == 'opus'
         file.save(filepath)
-    except:
+        with open(os.path.join(assets_path, 'contributions.cvs'), 'a') as contrib:
+            contrib.write(filename + '\n')
+        return ('OK', 200)
+    except AssertionError as e:
+        print(e)
         return ('ERROR', 500)
-
-    with open(os.path.join(assets_path, 'contributions.cvs'), 'a') as contrib:
-        contrib.write(filename + '\n')
-
-    return ('OK', 200)
